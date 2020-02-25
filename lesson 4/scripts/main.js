3dc8e237e7b50703ffae39c8ad088a40535da0d8
SHodapp117 <Hod13001@byui.edu>
2020-02-25 10:44:57 -0700

    More changes

--- lesson 4/scripts/main.js
+++ lesson 4/scripts/main.js
@@ -34,5 +34,5 @@ const hambutton = document.querySelector(".ham");
 hambutton.addEventListener("click", toggleMenu, false);
 
 function toggleMenu() {
-    document.querySelector(".navigation").classList.toggle("responsive");
+    document.querySelector(".navigation")[0].classList.toggle("responsive");
 }
