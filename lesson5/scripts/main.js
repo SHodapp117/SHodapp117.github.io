90b22323d4b804076273ce5f1cddb5231b2589ba
SHodapp117 <Hod13001@byui.edu>
2020-03-04 22:22:56 -0700

    Lesson five updates

--- lesson5/scripts/main.js
+++ lesson5/scripts/main.js
@@ -1,33 +1,34 @@
-
 // current date
 var cd = new Date();
 
-var daysOfWeek = [ 
-    'Sunday', 
-    'Monday', 
-    'Tuesday', 
-    'Wednesday', 
-    'Thursday', 
-    'Friday', 
-    'Saturday' ];
+var daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
 
 var months = [
-    'January', 
-    'February', 
-    'March', 
-    'April', 
-    'May', 
-    'June', 
-    'July', 
-    "August", 
-    'September', 
-    'October', 
-    'November', 
-    'December'];
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    "August",
+    'September',
+    'October',
+    'November',
+    'December'
+];
 
 // display full current date
-document.getElementById('currentDate').innerHTML = daysOfWeek[cd.getDay()] + ", " + cd.getDate() + " " + months[cd.getMonth()] + 
-" " + cd.getFullYear();
+document.getElementById('currentDate').innerHTML = daysOfWeek[cd.getDay()] + ", " + cd.getDate() + " " + months[cd.getMonth()] +
+    " " + cd.getFullYear();
 
 // hamburger menu
 const hambutton = document.querySelector(".ham");
@@ -47,5 +48,5 @@ const aside = document.querySelector('aside');
 
 if (currentDate.getDay() === 5) {
     aside.classList.remove('hidden');
- 
+
 }
